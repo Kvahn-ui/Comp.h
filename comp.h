8f29ff5636f69509811da97a0abff36b7548bb43
Kvahn-ui <87737885+Kvahn-ui@users.noreply.github.com>
2022-03-28 19:47:40 +1300

    compulsory.h
    the header file of the kings

---
+++ comp.h
@@ -0,0 +1,23 @@
+#pragma once
+ #ifdef __cplusplus
+ #include <iostream>
+ void hw() {
+    cout<<"Hello World!";
+ }
+ #else
+ #include <stdio.h>
+ #warning "you are cringe because you are not using c++"
+ #warning "C++ is just better so why in the fuck are you using c you fucking prehistoric shithead"
+ void hw() {
+    printf("Hello World!\n");
+ }
+ #endif
+ 
+
+#define hw hw()
+ #define mn main
+#define ret0 return 0
+#define ret1 return 1
+
+
+
